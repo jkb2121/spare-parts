3c8820c6ec7de2185b27971e1c81e7d3f6b3a83b
Jeff Brown <jkb@jkbworld.com>
2018-11-22 21:04:46 -0800

    Added the Product Variant Attributes building json to Spare Parts

--- json/build_json_product_variant_attributes.py
+++ json/build_json_product_variant_attributes.py
@@ -26,7 +26,6 @@ if write_product_variant_attributes_json:
 
             for product in pvav_json["products"]:
                 if product["prod_id"] == pvav[2]:
-                    print("Matched Prod_id")
                     product_found = True
 
                     variant_found = False
@@ -40,23 +39,33 @@ if write_product_variant_attributes_json:
 
                     if variant_found == False:
                         attribute = [{"id": pvav[3], "name": pvav[6], "value": pvav[9]}]
-                        variant = {"sku": pvav[11], "attributes": attribute}
+                        variant = {"sku": pvav[11], "type": "product", "attributes": attribute}
                         product["variants"].append(variant)
+
+                        if not {"id": pvav[3], "name": pvav[6]} in product["attribute_ids"]:
+                            product["attribute_ids"].append({"id": pvav[3], "name": pvav[6]})
+                        if not {"value": pvav[9]} in product["attribute_value_ids"]:
+                            product["attribute_value_ids"].append({"value": pvav[9]})
                     break
 
 
             if product_found == False:
 
+                attribute_ids = [{"id": pvav[3], "name": pvav[6]}]
+                attribute_value_ids = [{"value": pvav[9]}]
+
                 attribute = [{"id": pvav[3], "name": pvav[6], "value": pvav[9]}]
 
                 variant = [{"sku": pvav[11], "attributes": attribute}]
 
-                pvav_json["products"].append({"id": pvav[0], "name": pvav[1], "prod_id": pvav[2], "variants": variant}) #, ""values": [{"id": pvav[2], "name": pvav[3]}]})
+                pvav_json["products"].append({"id": pvav[0], "name": pvav[1], "prod_id": pvav[2], "type": "product",
+                                              "variants": variant, "attribute_ids": attribute_ids,
+                                              "attribute_value_ids": attribute_value_ids})
 
 
     print(pvav_json)
     pprint.pprint(pvav_json, depth=9, indent=5)
 
-    # # Write attributes_json to a file attributes.json
-    # with open("work/attribute_values.json", "w") as afw:
-    #     afw.write(json.dumps(attribute_values_json))
+    # Write attributes_json to a file attributes.json
+    with open("work/product_variant_attributes.json", "w") as afw:
+        afw.write(json.dumps(pvav_json))
